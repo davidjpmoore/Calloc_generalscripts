04bdd2268248059c7840c61caeddbdbb0e3afb2c
vlscaven <vlscaven@email.arizona.edu>
2015-08-24 14:21:30 -0700

    plots to assess for which years LAI data is available

--- reading in LAI.R
+++ reading in LAI.R
@@ -1,13 +1,46 @@
-##reading in Andrew Richardson's LAI data
+#reading in Andrew Richardson's LAI data
 LAI <- read.csv("~/Downloads/LAI%20for%20ELI.csv")
 View(LAI)
 summary(LAI)
 
-#looking at date range for single site at a time
-summary(LAI$ID[LAI$Site=="Ca-Oas"])
-LAI$ID[LAI$Site]
+#converting Year into character value
+LAI$chYear<-as.character(LAI$Year)
+summary(chYear)
+levels(chYear) #doesn't work to give range
 
 #formatting year.decimals as r-friendly dates
 library(lubridate)
 Date <- format(date_decimal(LAI$ID), "%m-%d-%Y")
 
+#plots to see years at each site
+###US-Ha1
+plot(LAI$Year[LAI$Site=="US-Ha1"], LAI$Site[LAI$Site=="US-Ha1"])
+###US-MMS
+plot(LAI$Year[LAI$Site=="US-MMS"], LAI$Site[LAI$Site=="US-MMS"])
+###US-UMB
+(plot(LAI$Year[LAI$Site=="US-UMB"], LAI$Site[LAI$Site=="US-UMB"])
+###US-WCr
+plot(LAI$Year[LAI$Site=="US-WCr"], LAI$Site[LAI$Site=="US-WCr"])
+ 
+#plots to see for which years LAI data is available
+###US-Ha1
+plot(LAI$Year[LAI$Site=="US-Ha1"],LAI$LAI2000_raw[LAI$Site=="US-Ha1"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+plot(LAI$Year[LAI$Site=="US-Ha1"],LAI$LAI2000_LAIr[LAI$Site=="US-Ha1"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+###US-MMS
+plot(LAI$Year[LAI$Site=="US-MMS"],LAI$LAI2000_raw[LAI$Site=="US-MMS"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+plot(LAI$Year[LAI$Site=="US-MMS"],LAI$LAI2000_LAIr[LAI$Site=="US-MMS"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+###US-UMB
+plot(LAI$Year[LAI$Site=="US-UMB"],LAI$LAI2000_raw[LAI$Site=="US-UMB"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+plot(LAI$Year[LAI$Site=="US-UMB"],LAI$LAI2000_LAIr[LAI$Site=="US-UMB"], xaxt='n')
+  axis(1, at=1990:2010, labels=NULL)
+###US-WCr
+plot(LAI$Year[LAI$Site=="US-WCr"],LAI$LAI2000_raw[LAI$Site=="US-WCr"])
+  axis(1, at=1998:2004, labels=NULL) #error: need finite ylim values
+plot(LAI$Year[LAI$Site=="US-WCr"],LAI$LAI2000_LAIr[LAI$Site=="US-WCr"], xaxt='n')
+  axis(1, at=1998:2004, labels=NULL) #error: need finite ylim values
+LAI$LAI2000_raw[LAI$Site=="US-WCr"] #all read "NA"; no LAI data available at this site?
